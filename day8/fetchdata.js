6b4fa8047fe738b5451f337aad550b6d16ac7632
nisarga <116147193+nisarga-k-a@users.noreply.github.com>
2025-02-20 20:59:32 +0530

    Delete day8/fetchdata.js

--- day8/fetchdata.js
+++
@@ -1,5 +0,0 @@
-const response = fetch('https://jsonplaceholder.typicode.com/users')
-response
-.then(response=>console.log(response.status))//if it is resolved
-.catch(err=>console.log(err))
-.finally(()=>console.log('Promise executed'))
