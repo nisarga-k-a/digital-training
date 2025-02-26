23ff2f45d9874636c2d9e2e5f0ed901d87db7838
nisarga <116147193+nisarga-k-a@users.noreply.github.com>
2025-02-26 09:50:39 +0530

    Add files via upload

---
+++ day11/shellScripting/file1.sh
@@ -0,0 +1,14 @@
+echo "hello world"
+
+name="nisarga"
+salary=80000
+
+echo "hello $name"
+echo "your salary is $salary"
+
+echo "your salary when it ia multiplied is $((3*salary))"
+
+num1=67
+num2=90
+
+echo "Sum is $((num1+num2))"
