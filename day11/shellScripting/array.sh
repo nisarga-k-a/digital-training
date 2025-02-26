d1b95641a72b808a3079be9ae5ed91b83f0ac2a2
nisarga <116147193+nisarga-k-a@users.noreply.github.com>
2025-02-26 09:48:01 +0530

    Add files via upload

---
+++ day11/shellScripting/array.sh
@@ -0,0 +1,7 @@
+numbers=(10 20 30 40 50)
+echo "First number ${numbers[0]}
+Second number ${numbers[1]}"
+
+names=("alex" "bob" "catty" "david")
+echo "First name ${names[0]} and Last name ${names[2]} "
+
